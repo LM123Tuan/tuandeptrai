bc7883dd4a452c0b978c168f22a0387cdf2f4e9b
LM123Tuan <hoimanchi888@gmail.com>
2024-01-05 19:53:54 +0700

    Buoi 3

--- JSA02/Buoi 2/Buoi_2.js
+++ JSA02/Buoi 2/Buoi_2.js
@@ -1,2 +1,36 @@
+// Vòng lặp
+var i = 1
+while (i < 5) {
+    console.log(i);
+    i = i + 1;
+}
 console.log("Hello");
 document.write("Hello")
+for (let j = 1; j < 5; j++) {
+    document.write(j)
+}
+// danh sách (mảng)
+const arr = ["Thành", "Tuấn", "Nguyên", "Dương", "Đức"]
+console.log(arr);
+for (let k = 0; k < arr.length; k += 1) {
+    console.log(arr[k]);
+}
+console.log("'Đảo ngược'")
+for (let k = 4; k >= 0; k -= 1) {
+    console.log(arr[k]);
+}
+// object (dictionary)
+var nhanvo = {
+    truong: "THPT LVT",
+    ten: "Anh Tuan",
+    lop: "11T",
+    mon: "jsa",
+    namhoc: "2023-2024",
+}
+console.log(nhanvo);
+console.log(nhanvo.ten);
+nhanvo.lop = "12T"
+console.log(nhanvo.lop);
+for (x in nhanvo) {
+    console.log(x + " , " + nhanvo[x]);
+}
