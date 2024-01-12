1a787dec7199a1b513e7f85a49a148e73d99e7ca
LM123Tuan <hoimanchi888@gmail.com>
2024-01-12 20:37:41 +0700

    Buoi 4

--- JSA02/Buoi 3/homework/homework_1.js
+++ JSA02/Buoi 3/homework/homework_1.js
@@ -1,8 +1,8 @@
-var n = 9
-var k = 0
+var n = 9;
+var k = 0;
 for (let i = 1; i <= 5; i++) {
-    let t = (n - i - k) / 2
-    let l = Math.floor(t)
-    document.writeln((l * " ") + ((i + k) * "1") + (l * " "))
+    let t = (n - i - k) / 2;
+    let l = Math.floor(t);
+    document.writeln(l * " " + (i + k) * "1");
     k += 1
 }
---
+++ JSA02/Buoi 4/app.js
@@ -0,0 +1,6 @@
+// lưu dữ liệu
+localStorage.setItem("username", "ricardo milos")
+// lấy dữ liệu
+console.log(localStorage.getItem("username"));
+// xóa dữ liệu
+localStorage.removeItem("lastname")
